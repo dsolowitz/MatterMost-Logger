248424b126776802771abe6464882af1d79deb88
david <ds438@duke.edu>
2018-11-16 12:09:32 -0500

    Test without Error Messages

--- src/main/java/LogTest.java
+++ src/main/java/LogTest.java
@@ -9,7 +9,6 @@ public class LogTest {
         logger.debug("This is a debug message");
         logger.info("This is an info message");
         logger.warn("This is a warn message");
-        logger.error("This is an error message of messages");
         logger.fatal("This is a fatal message");
 
     }
